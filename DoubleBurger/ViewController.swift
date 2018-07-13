5d4dec43658d8fa955493f1e1051d2376b173127
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-07-13 19:31:59 +0300

    Initial Commit

---
+++ DoubleBurger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoubleBurger
+//
+//  Created by ysoftware on 13.07.2018.
+//  Copyright © 2018 Ysoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
